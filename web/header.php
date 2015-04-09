dc0576842354fa833f4fc61d3f85cf8864b42b99
janjaap <j.j.m.vandeursen@gmail.com>
2015-04-09 08:49:53 +0200

    Changes page title

--- web/header.php
+++ web/header.php
@@ -2,13 +2,12 @@
 <html class="no-js">
 <head>
     <meta charset="utf-8">
-    <title>NS Techniek prototype</title>
+    <title>Page slide prototype | page <?= $page; ?></title>
 
     <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1.0">
 
     <link rel="stylesheet" href="/css/nst.css?v=<?php echo filemtime(__DIR__ . '/css/nst.css'); ?>">
 
-    <!--[if lte IE 9]><script src="/js/history.js"></script><![endif]-->
     <script src="/js/head.js?v=<?php echo filemtime(__DIR__ . '/js/head.js'); ?>" data-headjs-load="/js/nst.js?v=<?php echo filemtime(__DIR__ . '/js/nst.js'); ?>" async></script>
 </head>
 
